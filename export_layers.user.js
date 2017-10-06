626c01999b16ae382eb3be3d6125d2778e36d1f0
Jormund <etiennebotek@gmail.com>
2017-10-06 18:27:47 +0200

    Correct @includes

--- export_layers.user.js
+++ export_layers.user.js
@@ -2,18 +2,16 @@
 // @id             iitc-plugin-export-layers@Jormund
 // @name           IITC plugin: export layers 
 // @category       Layer
-// @version        0.1.0.201701005.1619
+// @version        0.1.1.201701006.1647
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @downloadURL    https://raw.githubusercontent.com/Jormund/export_layers/master/export_layers.user.js
-// @description    [2017-10-05-1619] Export layers from Layer chooser
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
+// @description    [2017-10-06-1647] Export layers from Layer chooser
+// @include        https://ingress.com/intel*
+// @include        http://ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
 // @grant          none 
 // ==/UserScript==
 
