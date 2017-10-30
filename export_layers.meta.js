3b5c44612aa4061d48c1ae661a967fba390674cf
Jormund <etiennebotek@gmail.com>
2017-10-30 18:15:35 +0100

    0.1.4.20171030.1633

--- export_layers.meta.js
+++ export_layers.meta.js
@@ -1,12 +1,12 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @id             iitc-plugin-export-layers@Jormund
 // @name           IITC plugin: export layers 
 // @category       Layer
-// @version        0.1.3.20171013.1850
+// @version        0.1.4.20171030.1633
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://raw.githubusercontent.com/Jormund/export_layers/master/export_layers.meta.js
 // @downloadURL    https://raw.githubusercontent.com/Jormund/export_layers/master/export_layers.user.js
-// @description    [2017-10-13-1850] Export layers from Layer chooser
+// @description    [2017-10-30-1633] Export layers from Layer chooser or search result
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
