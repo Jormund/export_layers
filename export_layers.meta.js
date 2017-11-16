4bee979257fa4572eab1172811f8a7d3ec3270fc
Jormund <etiennebotek@gmail.com>
2017-11-16 22:29:44 +0100

    download url changed to rawgit.com

--- export_layers.meta.js
+++ export_layers.meta.js
@@ -1,12 +1,12 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @id             iitc-plugin-export-layers@Jormund
 // @name           IITC plugin: export layers 
 // @category       Layer
-// @version        0.1.4.20171030.1633
+// @version        0.1.4.20171116.2228
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://raw.githubusercontent.com/Jormund/export_layers/master/export_layers.meta.js
-// @downloadURL    https://raw.githubusercontent.com/Jormund/export_layers/master/export_layers.user.js
-// @description    [2017-10-30-1633] Export layers from Layer chooser or search result
+// @updateURL      https://rawgit.com/Jormund/export_layers/master/export_layers.meta.js
+// @downloadURL    https://rawgit.com/Jormund/export_layers/master/export_layers.user.js
+// @description    [2017-11-16-2228] Export layers from Layer chooser or search result
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
