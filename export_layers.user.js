5575f824142e2bbf1a058912889ba7b86ddc0e62
Jormund <etiennebotek@gmail.com>
2017-12-04 22:21:09 +0100

    Fix export off normal layers broken with search result update

--- export_layers.user.js
+++ export_layers.user.js
@@ -1,12 +1,12 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @id             iitc-plugin-export-layers@Jormund
 // @name           IITC plugin: export layers 
 // @category       Layer
-// @version        0.1.4.20171116.2228
+// @version        0.1.4.20171204.2211
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://rawgit.com/Jormund/export_layers/master/export_layers.meta.js
 // @downloadURL    https://rawgit.com/Jormund/export_layers/master/export_layers.user.js
-// @description    [2017-11-16-2228] Export layers from Layer chooser or search result
+// @description    [2017-12-04-2211] Export layers from Layer chooser or search result
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
@@ -149,7 +149,7 @@ function wrapper(plugin_info) {
             if (layer.overlay) {
                 var resultItem = {};
                 resultItem.name = layer.name; //because we had to wrap search result, we wrap layer chooser too
-                resultItem.layer = layer;
+                resultItem.layer = layer.layer;
                 result.push(resultItem);
             }
         });
