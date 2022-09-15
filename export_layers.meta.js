5ba8931bdd0a0af54209b9355bb6f6b5a62f295c
Jormund <etiennebotek@gmail.com>
2022-09-15 23:07:32 +0200

    0.1.7 Handle multiple polygons in search result

--- export_layers.meta.js
+++ export_layers.meta.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-export-layers@Jormund
 // @name           IITC plugin: export layers 
 // @category       Layer
-// @version        0.1.6.20220901.1945
+// @version        0.1.7.20220915.2302
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://raw.githubusercontent.com/Jormund/export_layers/master/export_layers.meta.js
 // @downloadURL    https://raw.githubusercontent.com/Jormund/export_layers/master/export_layers.user.js
-// @description    [2022-09-01-1945] Export layers from Layer chooser or search result
+// @description    [2022-09-15-2302] Export layers from Layer chooser or search result
 // @include        https://*.ingress.com/intel*
 // @include        https://intel.ingress.com/*
 // @match          https://intel.ingress.com/*
